7a446b389e940989e860fb9de61df75bcc0faba0
Robin Wardle <robin.wardle@newcastle.ac.uk>
2025-06-19 18:03:13 +0100

    Added initial unittest test case

--- test/docker-suite/containers/client/run-tests.py
+++
@@ -1,5 +0,0 @@
-import json
-
-from data_warehouse_client import data_warehouse
-
-dw = data_warehouse.DataWarehouse("testdb-credentials.json", "testdb")
---
+++ test/docker-suite/containers/client/tests/connection.py
@@ -0,0 +1,42 @@
+# Copyright 2020 Newcastle University.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Python modules
+import unittest
+
+# Application modules
+from data_warehouse_client import data_warehouse
+
+# Test cases
+class TestConnection(unittest.TestCase):
+    """
+    """
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_db_connection(self):
+        dw = None
+        dw = data_warehouse.DataWarehouse("testdb-credentials.json","testdb")
+        unittest.unittest.assertIsNotNone(dw)
